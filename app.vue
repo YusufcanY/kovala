701c362788759d478346c613322d11ab9124c887
Yusufcan Yilmaz <playders03@gmail.com>
2022-08-31 01:54:09 +0300

    feat: loading screen added

--- app.vue
+++ app.vue
@@ -1,8 +1,14 @@
+<script setup lang="ts">
+  const isLoading = useState('isLoading', () => false)
+</script>
 <template>
-  <div class="grid h-screen grid-cols-16 p-2 pl-0">
-    <Sidebar class="col-span-3" />
-    <div class="col-span-13 h-full rounded-2xl bg-page-foreground">
-      <NuxtPage />
+  <div>
+    <div class="grid h-screen grid-cols-16 p-2 pl-0">
+      <Sidebar class="col-span-3" />
+      <div class="col-span-13 h-full rounded-2xl bg-page-foreground">
+        <NuxtPage />
+      </div>
     </div>
+    <LoadingScreen :is-loading="isLoading" />
   </div>
 </template>
