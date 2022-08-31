ec27d2553e89debb0786448885b5bc0a9f4ef682
Yusufcan Yilmaz <playders03@gmail.com>
2022-08-31 17:08:41 +0300

    feat: user box, lang (also flags) and theme added to sidebar

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -48,28 +48,31 @@
   ])
 </script>
 <template>
-  <div class="flex flex-col px-4">
-    <h2 class="py-4 text-center text-3xl font-black text-gray-800">KOVALA</h2>
-    <WorkspaceSelect />
-    <div class="mt-8 space-y-4">
-      <div v-for="(item, index) in navItems" :key="index" class="w-full">
-        <NavItemGroup v-if="item.children" :item-with-child="item">
-          <component :is="item.customComponent" v-if="item.customComponent" />
-        </NavItemGroup>
-        <NuxtLink
-          v-else
-          class="flex items-center space-x-2 rounded-2xl bg-primary-accent py-2 px-4 transition-all duration-300"
-          :class="
-            $route.path === item.route
-              ? 'bg-opacity-100 font-medium text-white'
-              : 'bg-opacity-0 hover:bg-opacity-10'
-          "
-          :to="item.route"
-        >
-          <component :is="item.icon" class="h-8 w-8" />
-          <span>{{ item.name }}</span>
-        </NuxtLink>
+  <div class="flex flex-col justify-between px-4">
+    <div class="flex flex-col">
+      <h2 class="py-4 text-center text-3xl font-black text-gray-800">KOVALA</h2>
+      <WorkspaceSelect />
+      <div class="mt-8 space-y-4">
+        <div v-for="(item, index) in navItems" :key="index" class="w-full">
+          <NavItemGroup v-if="item.children" :item-with-child="item">
+            <component :is="item.customComponent" v-if="item.customComponent" />
+          </NavItemGroup>
+          <NuxtLink
+            v-else
+            class="flex items-center space-x-2 rounded-2xl bg-primary-accent py-2 px-4 transition-all duration-300"
+            :class="
+              $route.path === item.route
+                ? 'bg-opacity-100 font-medium text-white'
+                : 'bg-opacity-0 hover:bg-opacity-10'
+            "
+            :to="item.route"
+          >
+            <component :is="item.icon" class="h-8 w-8" />
+            <span>{{ item.name }}</span>
+          </NuxtLink>
+        </div>
       </div>
     </div>
+    <User />
   </div>
 </template>
---
+++ components/User.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+  import { LanguageIcon, MoonIcon } from '@heroicons/vue/24/outline'
+</script>
+<template>
+  <div class="flex flex-col space-y-2">
+    <div class="grid grid-cols-2 gap-2">
+      <button
+        class="flex items-center space-x-2 rounded-lg p-2 hover:bg-gray-100"
+      >
+        <LanguageIcon class="h-6 w-6" />
+        <span class="font-semibold uppercase">{{ $i18n.locale }}</span>
+      </button>
+      <button
+        class="flex items-center space-x-2 rounded-lg p-2 hover:bg-gray-100"
+      >
+        <MoonIcon class="h-6 w-6" />
+        <span class="font-semibold">Theme</span>
+      </button>
+    </div>
+    <button
+      class="flex items-center space-x-2 rounded-lg border border-primary-accent border-opacity-0 p-4 transition-all duration-200 hover:border-opacity-100 hover:bg-gray-50"
+    >
+      <img alt="" class="h-10 w-10" src="/backend.png" />
+      <span class="font-bold">John Doe</span>
+    </button>
+  </div>
+</template>
