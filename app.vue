a22d05253b7a6262d7b152473113416bd515a5ce
Yusufcan Yilmaz <playders03@gmail.com>
2022-08-31 14:13:58 +0300

    feat: page transitions added

--- app.vue
+++ app.vue
@@ -13,3 +13,18 @@
     <NoMobileScreen />
   </div>
 </template>
+<style>
+  .page-enter-from {
+    opacity: 0;
+    transform: translateY(-10px);
+  }
+
+  .page-enter-active,
+  .page-leave-active {
+    transition: all 0.2s;
+  }
+  .page-enter,
+  .page-leave-to {
+    opacity: 0;
+  }
+</style>
