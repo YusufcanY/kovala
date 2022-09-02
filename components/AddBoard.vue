ec6c9777d53b2f36a8721ce99ac864eb8e1652fc
Yusufcan Yilmaz <playders03@gmail.com>
2022-09-02 21:26:59 +0300

    feat: ui improvements on sidebar

--- components/AddBoard.vue
+++ components/AddBoard.vue
@@ -4,7 +4,7 @@
 <template>
   <div>
     <button
-      class="flex w-full items-center space-x-2 rounded-lg border border-primary-accent border-opacity-0 bg-primary-accent bg-opacity-10 py-2 px-4 text-primary-accent transition-all duration-300 hover:border-opacity-100"
+      class="flex w-full items-center space-x-2 rounded-lg border-2 border-primary-accent border-opacity-0 bg-primary-accent bg-opacity-10 py-2 px-4 text-primary-accent transition-all duration-300 hover:border-opacity-100"
     >
       <PlusIcon class="h-6 w-6" />
       <span class="font-semibold">{{ $t('add_new') }}</span>
--- components/nav/ItemGroup.vue
+++ components/nav/ItemGroup.vue
@@ -31,13 +31,30 @@
         class="w-full"
       >
         <NuxtLink
-          class="group relative flex items-center space-x-2 rounded-lg bg-primary-accent bg-opacity-0 py-2 px-4 transition-all duration-300 hover:bg-opacity-10"
+          class="group relative flex items-center space-x-2 rounded-lg bg-primary-accent py-2 px-4 transition-all duration-300"
+          :class="
+            $route.path === child.route
+              ? 'font-medium text-white'
+              : 'bg-opacity-0 hover:bg-opacity-10'
+          "
           :to="child.route"
         >
           <div
-            class="absolute left-0 top-1/2 h-3 w-1 -translate-y-1/2 rounded-r-lg bg-primary-accent transition-all duration-200 group-hover:w-2"
+            class="absolute left-0 top-1/2 h-3 -translate-y-1/2 rounded-r-lg transition-all duration-200"
+            :class="
+              $route.path === child.route
+                ? 'w-2 bg-white opacity-60'
+                : 'w-1 bg-primary-accent group-hover:w-2'
+            "
           ></div>
-          <img class="h-6 w-6 p-1" :src="child.icon" />
+          <div
+            :class="{ 'rounded-full bg-white': $route.path === child.route }"
+          >
+            <img
+              class="h-6 w-6 p-1 transition-all duration-200"
+              :src="child.icon"
+            />
+          </div>
           <span>{{ child.name }}</span>
         </NuxtLink>
       </div>
