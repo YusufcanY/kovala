060b7c8d62b37dfe7900d0f4e6263c7127310024
Yusufcan Yilmaz <playders03@gmail.com>
2022-09-02 14:18:37 +0300

    feat: localization switch and turkish lang added

--- components/User.vue
+++ components/User.vue
@@ -4,16 +4,44 @@
     LanguageIcon,
     MoonIcon,
   } from '@heroicons/vue/24/outline'
+  import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
 </script>
 <template>
   <div class="flex flex-col space-y-2">
     <div class="grid grid-cols-2 gap-2">
-      <button
-        class="flex items-center space-x-2 rounded-lg p-2 hover:bg-gray-100"
-      >
-        <LanguageIcon class="h-6 w-6" />
-        <span class="font-semibold uppercase">{{ $i18n.locale }}</span>
-      </button>
+      <Menu as="div" class="relative">
+        <MenuButton
+          as="button"
+          class="flex w-full items-center space-x-2 rounded-lg p-2 transition-all duration-200 hover:bg-gray-100"
+        >
+          <LanguageIcon class="h-6 w-6" />
+          <span class="font-semibold uppercase">{{ $i18n.locale }}</span>
+        </MenuButton>
+        <MenuItems
+          class="absolute left-0 bottom-0 mb-12 w-full origin-bottom-left divide-y divide-gray-100 rounded-md bg-white shadow-lg"
+        >
+          <div class="px-1 py-1">
+            <MenuItem
+              v-for="(item, index) in $i18n.availableLocales"
+              :key="index"
+              v-slot="{ active }"
+            >
+              <button
+                :class="[
+                  { 'bg-gray-100': active },
+                  'flex w-full items-center space-x-1 rounded-md px-2 py-2 text-sm transition-all duration-200',
+                ]"
+                @click="$i18n.locale = item"
+              >
+                <img alt="" class="w-6" :src="`/flags/${item}-flag.png`" />
+                <span>
+                  {{ $t(item) }}
+                </span>
+              </button>
+            </MenuItem>
+          </div>
+        </MenuItems>
+      </Menu>
       <button
         class="flex items-center space-x-2 rounded-lg p-2 hover:bg-gray-100"
       >
--- plugins/i18n.ts
+++ plugins/i18n.ts
@@ -1,5 +1,6 @@
 import { createI18n } from 'vue-i18n'
 import en from '@/locales/en.json'
+import tr from '@/locales/tr.json'
 
 export default defineNuxtPlugin(({ vueApp }) => {
   const i18n = createI18n({
@@ -8,6 +9,7 @@ export default defineNuxtPlugin(({ vueApp }) => {
     locale: 'en',
     messages: {
       en,
+      tr,
     },
   })
 
