61f9c2ea815fe93eea02b86510e4c13147a56ef6
YusufcanY <playders03@gmail.com>
2022-12-27 11:47:33 +0300

    improvement: added meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,4 +25,30 @@ export default defineNuxtConfig({
       baseURL: 'https://res.cloudinary.com/dtenkljas/image/upload/v1672066966',
     },
   },
+  app: {
+    head: {
+      title: 'Kovala | Task Management App',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            'Kovala is a task management app that helps you manage your tasks and projects.',
+        },
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: 'Kovala | Task Management App',
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content:
+            'Kovala is a task management app that helps you manage your tasks and projects.',
+        },
+      ],
+    },
+  },
 })
