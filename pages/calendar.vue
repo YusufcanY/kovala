41a0292145ac9cdfbd006b478ddd00465429d909
YusufcanY <playders03@gmail.com>
2022-12-27 16:44:36 +0300

    improvement: added example calendar to calendar page

--- pages/calendar.vue
+++ pages/calendar.vue
@@ -1,6 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { CalendarView, CalendarViewHeader } from 'vue-simple-calendar'
+  import 'vue-simple-calendar/dist/style.css'
+
+  const items = ref([
+    {
+      id: 'e1',
+      title: 'Single-day item with a long title',
+      startDate: new Date(),
+      endDate: new Date(new Date().setDate(new Date().getDate() + 1)),
+    },
+  ])
+  const showDate = ref(new Date())
+  const setShowDate = (date: Date) => {
+    showDate.value = date
+  }
+</script>
 <template>
-  <div class="flex h-full items-center justify-center text-2xl font-bold">
-    calendar
+  <div>
+    <CalendarView
+      class="theme-default holiday-us-traditional holiday-us-official h-full"
+      :items="items"
+      :show-date="showDate"
+    >
+      <template #header="{ headerProps }">
+        <CalendarViewHeader :header-props="headerProps" @input="setShowDate" />
+      </template>
+    </CalendarView>
   </div>
 </template>
