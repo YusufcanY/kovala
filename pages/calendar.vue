91bfa18c51813740d962dd73af4fd446d363f8b7
YusufcanY <playders03@gmail.com>
2022-12-30 17:56:10 +0300

    feat: added button for adding items to calendar

--- pages/calendar.vue
+++ pages/calendar.vue
@@ -5,6 +5,7 @@
     ChevronRightIcon,
     ChevronUpDownIcon,
   } from '@heroicons/vue/20/solid'
+  import { ErrorMessage, Field, Form } from 'vee-validate'
   import {
     Listbox,
     ListboxButton,
@@ -13,7 +14,7 @@
     ListboxOptions,
   } from '@headlessui/vue'
   import 'vue-simple-calendar/dist/style.css'
-  import { MapPinIcon } from '@heroicons/vue/24/outline'
+  import { MapPinIcon, PlusIcon } from '@heroicons/vue/24/outline'
   const days = [
     { value: 0, label: 'Sun' },
     { value: 1, label: 'Mon' },
@@ -26,19 +27,19 @@
   const selectedStartOfWeek = ref(days[0])
   const items = ref([
     {
-      id: 'e1',
+      id: 1,
       title: 'Landing Hero Section',
       startDate: new Date(new Date().setDate(new Date().getDate() - 2)),
       endDate: new Date(new Date().setDate(new Date().getDate() + 1)),
     },
     {
-      id: 'e2',
+      id: 2,
       title: 'Just trying to get a long title for testing. ykwim',
       startDate: new Date(new Date().setDate(new Date().getDate() - 2)),
       endDate: new Date(new Date().setDate(new Date().getDate() + 1)),
     },
     {
-      id: 'e3',
+      id: 3,
       title:
         'That is looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo ooooooooooooooooooooooooooooooooooooooooong text.',
       startDate: new Date(new Date().setDate(new Date().getDate() - 6)),
@@ -49,9 +50,20 @@
   const selectedPeriod = ref('month')
   const changeTopCalcFuntionForItem = (calc: string) =>
     calc.replace('2px', '12px')
+
+  const isModal = ref(false)
+  const onSubmit = (values: any) => {
+    items.value.push({
+      id: items.value.length + 1,
+      title: values.title,
+      startDate: new Date(),
+      endDate: new Date(),
+    })
+    isModal.value = false
+  }
 </script>
 <template>
-  <div class="h-full p-6">
+  <div class="relative flex h-full p-6">
     <CalendarView
       class="h-full rounded-xl bg-white p-4 dark:bg-dark-page-body [&_*]:!hide-scrollbar [&>div]:!border-none [&>div_*]:!border-page-foreground [&>div_*]:dark:!border-dark-foreground"
       :display-period-uom="selectedPeriod"
@@ -234,6 +246,54 @@
         </div>
       </template>
     </CalendarView>
+    <button
+      class="absolute bottom-14 right-14 flex aspect-square items-center justify-center rounded-md bg-primary-accent p-2 text-white shadow-primary-blurred transition-all duration-200"
+      @click="isModal = true"
+    >
+      <PlusIcon class="h-10 w-10" />
+    </button>
+    <Modal v-model="isModal">
+      <template #title>
+        {{ $t('new-board-group') }}
+      </template>
+      <Form v-slot="{ errors, meta }" class="mt-8 space-y-4" @submit="onSubmit">
+        <div class="flex flex-col">
+          <label for="name">{{ $t('name') }}</label>
+          <Field
+            id="title"
+            class="mt-1 rounded-md bg-page-foreground p-2 ring-primary-accent transition-all duration-200 focus:ring-1 dark:bg-dark-foreground"
+            :class="!errors.title ? 'ring-primary-accent' : 'ring-red-500'"
+            :label="$t('title')"
+            name="title"
+            rules="required|max:128|alpha"
+            type="text"
+            validate-on-input
+          />
+          <div class="relative h-4">
+            <ErrorMessage
+              class="absolute top-0 left-0 text-xs font-semibold text-red-500"
+              name="title"
+            />
+          </div>
+        </div>
+        <div class="grid grid-cols-2 gap-4">
+          <button
+            class="rounded-md bg-red-600 bg-opacity-0 py-2 font-semibold transition-all duration-200 hover:bg-opacity-100 hover:text-white"
+            type="button"
+            @click="isModal = false"
+          >
+            {{ $t('cancel') }}
+          </button>
+          <button
+            class="rounded-md bg-primary-accent py-2 font-bold text-white ring-1 ring-primary-accent transition-all duration-200 hover:shadow-primary-blurred disabled:cursor-not-allowed disabled:opacity-50 dark:bg-opacity-10 dark:hover:bg-opacity-100 dark:hover:shadow-none dark:hover:disabled:bg-opacity-10"
+            :disabled="!(meta.valid && meta.dirty)"
+            type="submit"
+          >
+            {{ $t('create') }}
+          </button>
+        </div>
+      </Form>
+    </Modal>
   </div>
 </template>
 <style scoped>
