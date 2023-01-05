9df861a3a92046c5ed8e20fbb01db4a9a8e85369
YusufcanY <playders03@gmail.com>
2023-01-05 13:51:58 +0300

    fix: validator bug fixed

--- plugins/veevalidate.ts
+++ plugins/veevalidate.ts
@@ -1,13 +1,25 @@
 import { configure, defineRule } from 'vee-validate'
-import AllRules from '@vee-validate/rules'
+import {
+  alpha,
+  alpha_spaces as alphaSpaces,
+  max,
+  regex,
+  required,
+} from '@vee-validate/rules'
 import { localize } from '@vee-validate/i18n'
 import en from '@vee-validate/i18n/dist/locale/en.json'
 import tr from '@vee-validate/i18n/dist/locale/tr.json'
 
 export default defineNuxtPlugin(() => {
-  Object.keys(AllRules).forEach((rule) => {
+  /*   Object.keys(AllRules).forEach((rule) => {
     defineRule(rule, AllRules[rule])
-  })
+  }) */
+  defineRule('required', required)
+  defineRule('max', max)
+  defineRule('alpha', alpha)
+  defineRule('alpha_spaces', alphaSpaces)
+  defineRule('regex', regex)
+
   configure({
     generateMessage: localize({
       en,
